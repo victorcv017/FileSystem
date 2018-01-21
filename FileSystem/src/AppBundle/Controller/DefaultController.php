772e6326ccaea0807bd850757bb8a19db661cd6a
victor.cv <victor.vasconceloj91@anahuac.com>
2018-01-21 02:11:00 -0600

    Login

--- FileSystem/src/AppBundle/Controller/DefaultController.php
+++ FileSystem/src/AppBundle/Controller/DefaultController.php
@@ -2,18 +2,54 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
-{
+{   
+    public function signInAction(){
+        if ($this->getUser() instanceof User) {
+            
+            return $this->redirectToRoute('profile');
+        }
+        $aux = $this->get('security.authentication_utils');
+        $last_username = $aux->getLastUsername();
+
+        return $this->render('@App/sign_in.html.twig', array(
+                    'last_username' => $last_username
+        ));
+    }
     
-    public function exampleAction(Request $request)
-    {
-        echo "holaaa";
-        return $this->render('@App/example.html.twig', array(
+    public function profileAction(Request $request){
+        $user = $this->getUser();
+        
+        return $this->render('@App/profile.html.twig', array(
                     
         ));
     }
+    
+    public function signUpAction(Request $request) {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $encoder = $this->get('security.password_encoder');
+            $data->setPassword($encoder->encodePassword($data, $data->getPassword()));
+            //var_dump($data->getCorreo());
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($data);
+            $em->flush();
+            return $this->redirectToRoute('signIn');
+        }
+        
+        return $this->render('@App/sign_up.html.twig', array(
+                    'form' => $form->createView()
+                        // ...
+        ));
+    }
+
 }
---
+++ FileSystem/src/AppBundle/Form/UserType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use \Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use \Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', TextType::class)
+                ->add('email', EmailType::class)
+                ->add('password', RepeatedType::class, array(
+                    'type' => PasswordType::class,
+                    'invalid_message' => 'Las contraseñas deben coincidir.',
+                    'options' => array('attr' => array('class' => 'password-field')),
+                    'required' => true,
+                    'first_options' => array('label' => 'Password'),
+                    'second_options' => array('label' => 'Confirmar Password'),
+                ))->add('registrar', SubmitType::class);
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_user';
+    }
+
+
+}
